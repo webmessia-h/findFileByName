7b4a9ed87be18a284bbada0b97b433b004d3f342
webmessiah <110561823+webmessia-h@users.noreply.github.com>
2024-03-01 21:46:20 +0200

    comments for ThreadPool.cpp

--- src/ThreadPool.cpp
+++ src/ThreadPool.cpp
@@ -1,42 +1,42 @@
 #include "ThreadPool.hpp"
 
-ThreadPool::ThreadPool(int numThr) : stop(false)
+ThreadPool::ThreadPool(int numThr) : stop(false)    // create fixed quantity of threads
 {
     for (int i = 0; i < numThr; i++)
     {
-        threads.emplace_back([this]
+        threads.emplace_back([this]    // [this] to access variables of enclosed class
         {
              while (true)
              {
-                 std::function<void()> task;
+                 std::function<void()> task;    // create one empty task
                  {
                      std::unique_lock<std::mutex> lock(qMutex);
-                     cond.wait(lock, [this] { return stop || !tasks.empty(); });
+                     cond.wait(lock, [this] { return stop || !tasks.empty(); });    // if tasks not empty nor stop is true 
                      if (stop && tasks.empty())
                      {
                          return;
                      }
 
-                     task = std::move(tasks.front());
-                     tasks.pop();
+                     task = std::move(tasks.front());    // assign task from queue
+                     tasks.pop();    // delete task from tasks queue
                  }
-                 task();
+                 task(); 
              }
         });
     }
 }
 
-ThreadPool::~ThreadPool()
+ThreadPool::~ThreadPool()    // destructor 
 {
     {
         std::unique_lock<std::mutex> lock(qMutex);
-        stop = true;
+        stop = true;    
     }
-    cond.notify_all();
+    cond.notify_all();    
 
     for (std::thread &worker : threads)
     {
-        worker.join();
+        worker.join();    
     }
 }
 
