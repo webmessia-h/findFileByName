3a6283f465312ae3e1aeb4bc0e17c6f7de529107
webmessiah <110561823+webmessia-h@users.noreply.github.com>
2024-03-01 21:39:24 +0200

    comments for ThreadPool.hpp

--- src/ThreadPool.hpp
+++ src/ThreadPool.hpp
@@ -8,27 +8,27 @@
 class ThreadPool
 {
 private:
-    std::queue<std::function<void()>> tasks;
-    std::vector<std::thread> threads;
+    std::queue<std::function<void()>> tasks;    // queue of void-function objects to assing task to
+    std::vector<std::thread> threads;    // vector of threads 
 
-    std::condition_variable cond;
-    std::mutex qMutex;
+    std::condition_variable cond;    // condition variable to tell threads that queue is reachable to take task from
+    std::mutex qMutex;    // to block acces to the queue for more than one thread simultaneously
     bool stop;
 
 public:
-    explicit ThreadPool(int numThr);
+    explicit ThreadPool(int numThr);    // create Thread Pool
     ~ThreadPool();
 
     template <class F>
-    void enqueue(F &&task)
+    void enqueue(F &&task)    // function to enqueue tasks (pass by reference)
     {
         {
-            std::unique_lock<std::mutex> lock(qMutex);
+            std::unique_lock<std::mutex> lock(qMutex);       
             tasks.emplace(std::forward<F>(task));
         }
 
-        cond.notify_one();
+        cond.notify_one();    // notify one thread about waiting task
     }
 
-    void stopped();
+    void stopped();    
 };
