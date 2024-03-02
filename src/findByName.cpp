667ffa4a5f51cf4d67cc792d7e6e3962f02aca9d
webmessiah <110561823+webmessia-h@users.noreply.github.com>
2024-03-02 16:11:32 +0200

    Update findByName.cpp

--- src/findByName.cpp
+++ src/findByName.cpp
@@ -30,7 +30,7 @@ void FindByName::search()
     fs::path path = fs::path("/");    // path to start search from, adjustable, better use "/home/username" for safety reasons 
     
     // count directories in current location
-    int directoryCount = std::count_if(   
+    int directoryCount = std::count_if (   
             fs::directory_iterator(path),
             fs::directory_iterator(),
             [](const fs::directory_entry &entry)
@@ -39,7 +39,7 @@ void FindByName::search()
             });
 
     // if amount of directories then create maximum amount of threads (adjustable), else create corresponding amount of threads
-    (directoryCount > MAX_TREADS) ? t = std::make_shared<ThreadPool>(MAX_TREADS) : t = std::make_shared<ThreadPool>(directoryCount);
+    (directoryCount > MAX_TREADS)    ?    t = std::make_shared<ThreadPool>(MAX_TREADS)    :    t = std::make_shared<ThreadPool>(directoryCount);
 
     // for every directory enque task of searching for file in it
     for (const auto &dir : fs::directory_iterator(path))
